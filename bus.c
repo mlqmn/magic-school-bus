d1079aa662aff3eb01d1f340ee1cf11c8e910137
SebastianHYH <sebastianhung25@gmail.com>
2026-09-21 23:19:10 +0700

    add: bus.c. Src folder not required turns out (maybe)

---
+++ bus.c
@@ -0,0 +1,19 @@
+#include "SIMLIB/simlib.h"      /* Required for use of simlib.c. */
+
+#define EVENT_ARRIVAL       1   /* Event type for arrival of a job to the bus. */
+#define EVENT_DEPARTURE     2   /* Event type for departure of a job from the bus. */
+#define LIST_QUEUE          1   /* List number for bus queue. */
+#define LIST_BUS            2   /* List number for bus. */
+#define SAMPST_DELAYS       1   /* sampst variable for delays in queue. */
+#define STREAM_INTERARRIVAL 1   /* Random-number stream for interarrivals. */
+#define STREAM_SERVICE      2   /* Random-number stream for service times. */
+
+/* Declare non-simlib global variables. */
+
+/* Declare non-simlib functions. */
+
+
+
+int main() {
+    
+}
